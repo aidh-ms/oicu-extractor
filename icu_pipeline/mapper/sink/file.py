8f9ffa655584f250cf5ce3dc020e110d2c9f79a4
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-12-17 19:16:42 +0100

    fix header in csv file sink

--- icu_pipeline/mapper/sink/file.py
+++ icu_pipeline/mapper/sink/file.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 from pandera.typing import DataFrame
 
@@ -6,11 +8,22 @@ from icu_pipeline.mapper.sink import AbstractSinkMapper
 
 
 class CSVFileSinkMapper(AbstractSinkMapper):
+    def __init__(self, path: Path | None = None) -> None:
+        super().__init__()
+
+        self._path = path or Path(".")
+
     def to_output_format(
         self,
         df: DataFrame[AbstractSinkSchema],
         schema: AbstractSinkSchema,
     ) -> None:
+        file_path = self._path / f"{schema._SINK_NAME}.csv"
+
+        header = False
+        if not file_path.exists():
+            header = True
+
         for column in df.columns:
             if not isinstance(df[column][0], dict):
                 continue
@@ -18,4 +31,4 @@ class CSVFileSinkMapper(AbstractSinkMapper):
             df = df.join(pd.json_normalize(df[column]).add_prefix(f"{column}__"))
             df = df.drop(columns=[column])
 
-        df.to_csv(f"{schema._SINK_NAME}.csv", mode="a+", index=False)
+        df.to_csv(file_path, mode="a+", index=False, header=header)

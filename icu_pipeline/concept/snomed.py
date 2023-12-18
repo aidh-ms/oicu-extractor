99844fab2f8f247f713263885b72d48a607b8000
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2023-12-18 21:37:57 +0100

    add chartevent mapper

--- icu_pipeline/concept/snomed.py
+++ icu_pipeline/concept/snomed.py
@@ -3,6 +3,7 @@ from icu_pipeline.mapper.schema.fhir import FHIRObservation
 from icu_pipeline.mapper.schema.ohdsi import AbstractOHDSISinkSchema
 from icu_pipeline.mapper.source import DataSource
 from icu_pipeline.mapper.source.mimic.labevents import MimicSerumCreatinineMapper
+from icu_pipeline.mapper.source.mimic.chartevent import MimicHeartRateMapper
 
 
 class SerumCreatinine(AbstractSnomedConcept):
@@ -10,3 +11,10 @@ class SerumCreatinine(AbstractSnomedConcept):
     FHIR_SCHEMA = FHIRObservation
     OHDSI_SCHEMA = AbstractOHDSISinkSchema
     MAPPER = {DataSource.MIMIC: MimicSerumCreatinineMapper}
+
+
+class HeartRate(AbstractSnomedConcept):
+    SNOMED_ID = "364075005"
+    FHIR_SCHEMA = FHIRObservation
+    OHDSI_SCHEMA = AbstractOHDSISinkSchema
+    MAPPER = {DataSource.MIMIC: MimicHeartRateMapper}

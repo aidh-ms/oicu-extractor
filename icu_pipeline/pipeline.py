7ed26031eef12cc86d84f8a06a52b18fca34ce97
aegis301 <57251443+aegis301@users.noreply.github.com>
2023-12-28 20:50:00 +0100

    chore: add test.py

--- icu_pipeline/pipeline.py
+++ icu_pipeline/pipeline.py
@@ -7,6 +7,34 @@ from icu_pipeline.mapper.source import SourceMapperConfiguration, DataSource
 
 
 class Pipeline:
+    """
+    The main class used to represent the Pipeline for data processing.
+
+    The Pipeline class is used to represent the pipeline for data processing. It is
+    used for processing a list of concepts in parallel, using a pool of worker
+    processes.
+
+    Attributes
+    ----------
+    _concepts : list[Type[AbstractConcept]]
+        a list of concepts to be processed
+    _sink_mapper : AbstractSinkMapper
+        a mapper that defines how the data is to be written out
+    _mapping_format : MappingFormat
+        the format of the mapping
+    _source_mapper_configs : dict[DataSource, SourceMapperConfiguration]
+        a dictionary mapping data sources to their configurations
+    _processes : int, optional
+        the number of worker processes. Defaults to 2.
+
+    Methods
+    -------
+    _worker_func(concept: AbstractConcept)
+        The function to be run by each worker process.
+    transfrom()
+        Executes the pipeline, processing each concept in parallel.
+    """
+
     def __init__(
         self,
         concepts: list[Type[AbstractConcept]],
@@ -22,9 +50,15 @@ class Pipeline:
         self._processes = processes
 
     def _worker_func(self, concept: AbstractConcept):
+        """
+        The function to be run by each worker in the pool.
+        """
         concept.map()
 
     def transfrom(self):
+        """
+        Executes the pipeline, processing each concept in parallel.
+        """
         with Pool(processes=self._processes) as worker_pool:
             concepts = [
                 concept(

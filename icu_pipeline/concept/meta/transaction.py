0c0d740f0f166ddafd7f739927d8e231fa07b9e0
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-01-14 13:42:16 +0100

    add encounter

--- icu_pipeline/concept/meta/transaction.py
+++ icu_pipeline/concept/meta/transaction.py
@@ -2,11 +2,15 @@ from icu_pipeline.concept.meta import AbstractMetaConcept
 from icu_pipeline.mapper.schema.fhir.encounter import FHIREncounter
 from icu_pipeline.mapper.schema.ohdsi import AbstractOHDSISinkSchema
 from icu_pipeline.mapper.source import DataSource
-from icu_pipeline.mapper.source.mimic.icustays import ICUEncounterMapper
+from icu_pipeline.mapper.source.mimic.icustays import MimicICUEncounterMapper
+from icu_pipeline.mapper.source.eicu.patient import eICUEncounterMapper
 
 
 class ICUEncounter(AbstractMetaConcept):
     CONCEPT_ID = "icu_encounter"
     FHIR_SCHEMA = FHIREncounter
     OHDSI_SCHEMA = AbstractOHDSISinkSchema
-    MAPPER = {DataSource.MIMIC: ICUEncounterMapper}
+    MAPPER = {
+        # DataSource.MIMIC: MimicICUEncounterMapper,
+        DataSource.EICU: eICUEncounterMapper,
+    }
---
+++ icu_pipeline/mapper/source/utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+from icu_pipeline.mapper.schema.fhir import Period
+
+
+def to_timestamp(
+    time: str, year: int | str, month: int | str = "01", day: int | str = "01"
+) -> pd.Timestamp:
+    return pd.Timestamp(f"{year}-{month}-{day}T{time}")
+
+
+def offset_to_timestamp(timestamp: pd.Timestamp, offset: int) -> pd.Timestamp:
+    return timestamp + pd.Timedelta(minutes=offset)
+
+
+def offset_to_period(
+    admit_year: int | str, admit_time: str, admit_offset: int, discharge_offset: int
+) -> Period:
+    admit_timestamp = to_timestamp(admit_time, admit_year)
+    start = offset_to_timestamp(admit_timestamp, admit_offset)
+    return Period(
+        start=start,
+        end=offset_to_timestamp(start, discharge_offset),
+    )

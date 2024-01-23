a72967dc40aa7c9e7e729bbb778214731906e1dd
aegis301 <57251443+aegis301@users.noreply.github.com>
2024-01-23 10:46:54 +0100

    feat: add dotenv support

--- test_mimic.py
+++ test_mimic.py
@@ -61,11 +61,23 @@ from icu_pipeline.concept.snomed.observable_entity import (
     Temperature,
     UrineOutput,
 )
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    MIMIC_DB = os.getenv("MIMIC_DB")
+    AMDS_DB = os.getenv("AMDS_DB")
+    EICU_DB = os.getenv("EICU_DB")
+
     configs = {
         DataSource.MIMIC: SourceMapperConfiguration(
-            "postgresql+psycopg://christian@localhost/mimiciv"
+            f"postgresql+psycopg://{POSTGRES_USER}@{POSTGRES_HOST}/{MIMIC_DB}",
         ),
     }
 

da7d2abc7de8c58ffce1333f2b0d4fcae31348ba
aegis301 <57251443+aegis301@users.noreply.github.com>
2024-05-13 22:35:36 +0200

    chore: remove concepts folder

--- icu_pipeline/concepts/snomed/__init__.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta
-from typing import Type
-
-from icu_pipeline.concepts import AbstractConcept
-from icu_pipeline.mapper.source import AbstractSourceMapper, DataSource
-
-
-class AbstractSnomedConcept(AbstractConcept, metaclass=ABCMeta):
-    CONCEPT_TYPE = "snomed"

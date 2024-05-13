64d9bc61c2687b5016750629777c76ab37a1ef4b
aegis301 <57251443+aegis301@users.noreply.github.com>
2024-05-13 21:32:59 +0200

    fix: working testing file

---
+++ test_yaml_mimic.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from icu_pipeline.pipeline import (
+    Pipeline,
+    DataSource,
+    MappingFormat,
+    SourceMapperConfiguration,
+)
+from icu_pipeline.mapper.sink.file import CSVFileSinkMapper
+
+
+if __name__ == "__main__":
+    configs = {
+        DataSource.MIMIC: SourceMapperConfiguration()
+    }
+    concept_paths = [*(Path(__file__).parent / "concepts").rglob("*.yaml")]
+
+    pipeline = Pipeline(
+        configs,
+        CSVFileSinkMapper(),
+        MappingFormat.FHIR,
+    )
+
+    pipeline.transform(["HeartRate.yml"])

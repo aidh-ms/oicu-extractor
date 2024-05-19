6ee71f5f566466464002a291470b2602946cf551
aegis301 <christian.porschen@gmail.com>
2024-05-19 16:53:50 +0200

    test: add test for file logging

--- icu_pipeline/logger.py
+++ icu_pipeline/logger.py
@@ -2,10 +2,10 @@ import logging
 import colorlog
 
 
-class ICUPLogger:
+class ICULogger:
     def __init__(self):
         self._logger: logging.Logger = logging.getLogger()  # Get the root logger
-        self._logger.setLevel(logging.INFO)  # Set your desired log level
+        self._logger.setLevel(logging.DEBUG)  # Set your desired log level
 
         formatter = colorlog.ColoredFormatter(
             '%(log_color)s%(levelname)s:%(name)s:%(message)s%(reset)s',
@@ -20,8 +20,11 @@ class ICUPLogger:
 
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
-
         self._logger.addHandler(console_handler)
+
+        file_handler = logging.FileHandler("logs/icu_pipeline.log")
+        file_handler.setFormatter(formatter)
+        self._logger.addHandler(file_handler)
 
     def get_logger(self):
         return self._logger

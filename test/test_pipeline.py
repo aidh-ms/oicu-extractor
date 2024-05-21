8e11a76bce4f286169e5d7a7cc0620ead6f3afa4
aegis301 <christian.porschen@gmail.com>
2024-05-21 08:02:05 +0200

    test: restruct and add pipeline run

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -1,23 +1,50 @@
 import pytest
-from icu_pipeline.pipeline import Pipeline, DataSource, SourceMapperConfiguration
+import os
+from icu_pipeline.pipeline import Pipeline, DataSource, SourceMapperConfiguration, MappingFormat
 from icu_pipeline.mapper.sink.file import CSVFileSinkMapper
 
 
-@pytest.fixture
-def setup_pipeline():
-    source_configs = {DataSource.MIMIC: SourceMapperConfiguration()}
-    sink_mapper = CSVFileSinkMapper()
-    pipeline = Pipeline(source_configs, sink_mapper)
-    example_concept = pipeline._load_concepts(["HeartRate"])
-    return pipeline, example_concept
+class TestPipeline:
+    @pytest.fixture
+    def setup_pipeline(self):
+        source_configs = {DataSource.MIMIC: SourceMapperConfiguration()}
+        sink_mapper = CSVFileSinkMapper()
+        pipeline = Pipeline(source_configs, sink_mapper)
+        example_concept = pipeline._load_concepts(["HeartRate"])
+        return pipeline, example_concept
 
+    def test_load_concepts(self, setup_pipeline):
+        """
+        Test for the _load_concepts method of the Pipeline class.
+        Asserts that the method can successfully load the concepts "HeartRate" and "SystolicBloodPressure".
+        """
+        pipeline = setup_pipeline[0]
+        assert pipeline._load_concepts(["HeartRate", "SystolicBloodPressure"])
 
-def test_load_concepts(setup_pipeline):
-    pipeline = setup_pipeline[0]
-    assert pipeline._load_concepts(["HeartRate", "SystolicBloodPressure"])
+    def test_worker_func(self, setup_pipeline):
+        """
+        Test for the _worker_func method of the Pipeline class.
+        Asserts that the method is implemented and can process the example concept.
+        """
+        pipeline, example_concepts = setup_pipeline
+        assert pipeline._worker_func(example_concepts[0])
 
+    def test_run(self, setup_pipeline):
+        """
+        Test run for the whole pipeline implementation
+        Asserts that the pipeline can be successfully executed.
+        """
+        configs = {
+            DataSource.MIMIC: SourceMapperConfiguration()
+        }
 
-def test_worker_func(setup_pipeline):
-    # assert the function is implemented
-    pipeline, example_concepts = setup_pipeline
-    assert pipeline._worker_func(example_concepts[0])
+        pipeline = Pipeline(
+            configs,
+            CSVFileSinkMapper(),
+            MappingFormat.FHIR,
+        )
+
+        pipeline.transform(["HeartRate"])
+        # assert that the output is correctly created on root directory
+        assert os.path.exists("output")
+        assert os.path.exists("output/HeartRate.csv")

fa36ee5d7f4f03ef286328fd4892552c577c4245
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2024-10-03 17:01:50 +0200

    add length unit

---
+++ icu_pipeline/unit/length.py
@@ -0,0 +1,54 @@
+from typing import Callable
+from pandera.typing import Series, DataFrame
+from icu_pipeline.schema.fhir import Quantity
+from icu_pipeline.unit.converter import BaseConverter
+
+
+class LengthConverter(BaseConverter):
+    SI_UNIT = "m"
+    AVAILABLE_UNITS = ["cm", "m"]
+
+    def _convertToSI(
+        self,
+        source_unit: str,
+        data: Series[Quantity],
+        dependencies: dict[str, DataFrame],
+    ):
+        convert: Callable[[float], float] = lambda v: v
+        # Data can have any Unit and will be transformed to m
+        match source_unit:
+            # Already SI-Unit
+            case self.SI_UNIT:
+                return data
+
+            # Actual Conversions from case to cm
+            case "cm":
+                convert = lambda v: v * 100
+
+            # Not Implemented
+            case _:
+                raise NotImplementedError
+
+        return data.apply(
+            lambda q: Quantity(value=convert(q["value"]), unit=self.SI_UNIT)
+        )
+
+    def _convertToTarget(
+        self, sink_unit: str, data: Series[Quantity], dependencies: dict[str, DataFrame]
+    ):
+        convert: Callable[[float], float] = lambda v: v
+        # Data uses m and can be transformed in to any Unit
+        match sink_unit:
+            # Already SI-Unit
+            case self.SI_UNIT:
+                return data
+
+            # Actual Conversions
+            case "cm":
+                convert = lambda v: v / 100
+
+            # Not Implemented
+            case _:
+                raise NotImplementedError
+
+        return data.apply(lambda q: Quantity(value=convert(q["value"]), unit=sink_unit))
